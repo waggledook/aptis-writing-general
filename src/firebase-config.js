9f7de9b535581a27dfa86cbf8135ec3fe25a8fa8
waggledook <waggledook@gmail.com>
2026-04-08 13:20:47 +0200

    tried to fix this again

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,24 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCvpE87D16safq68oFB4fJKPyCURsc-mrU",
+  authDomain: "examplay-auth.firebaseapp.com",
+  projectId: "examplay-auth",
+  storageBucket: "examplay-auth.firebasestorage.app",
+  messagingSenderId: "654835226958",
+  appId: "1:654835226958:web:a95cd8da4adb09c8a5661f",
+  measurementId: "G-DMMT8D3XBR",
 };
-
-const missingFirebaseVars = Object.entries(firebaseConfig)
-  .filter(([, value]) => !value)
-  .map(([key]) => key);
-
-if (missingFirebaseVars.length) {
-  throw new Error(
-    `Missing Firebase environment variables: ${missingFirebaseVars.join(", ")}`
-  );
-}
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
